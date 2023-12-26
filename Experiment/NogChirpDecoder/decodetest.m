44d44a7b33654910db2d7724564c374a0081538e
ryonck <ryon_ck@outlook.com>
2023-12-26 15:47:56 +0800

    Add decodetest

---
+++ Experiment/NogChirpDecoder/decodetest.m
@@ -0,0 +1,24 @@
+% 测试CHchirpDecoder
+clear;
+fclose all;     %关闭所有matlab打开的文件
+tic;            % 打开计时器
+
+% 读取配置和验证文件
+sf = 10;
+bw = 125e3;
+samplesRate = 2e6;
+[loraSet] = readLoraSet('GeneralConfig.json', sf, bw, samplesRate);
+loraSet.payloadNum = 25; % payload数目
+NogChirpDecoder = NogChirpDecoder(loraSet);
+% 读取文件夹下所有采样值文件
+fileDir = '\\192.168.3.102\e\data\channel2_231220\';
+
+% 从文件中读取信号流
+[signal] = readSignalFile(fileDir, fileIn(2));
+
+NogChirpDecoder = NogChirpDecoder.decode(signal);
+
+disp(NogChirpDecoder.binRecord);
+
+toc;
+fclose all;
